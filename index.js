22ad1ad8a90a583f6759ab86f9372e341d1f92b7
mbcse <mbcse50@gmail.com>
2025-02-05 00:48:35 +0530

    Fix

--- index.js
+++ index.js
@@ -171,7 +171,7 @@ io.on('connection', (socket) => {
     });
 });
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT ||  8080;
 server.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
