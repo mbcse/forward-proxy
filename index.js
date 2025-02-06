cdf260deaf50b0a9f0e2db871e6f034a57c29750
mbcse <mbcse50@gmail.com>
2025-02-06 21:16:42 +0530

    Removed twiimg subdomain

--- index.js
+++ index.js
@@ -15,8 +15,8 @@ const ALLOWED_DOMAINS = [
     'twitter.com',
     'x.com',
     'api.twitter.com',
-    'pbs.twimg.com',
-    'video.twimg.com',
+    'twimg.com',
+    'twimg.com',
     'whatismyipaddress.com'
 ];
 
@@ -183,7 +183,7 @@ const server = http.createServer((req, res) => {
             followRedirects: true,
             changeOrigin: true,
             headers: {
-                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36',
+                'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Safari/537.36',
                 'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
                 'Accept-Language': 'en-US,en;q=0.9',
                 'Connection': 'keep-alive'
